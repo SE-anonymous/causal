dfd5947191da80df3505b5c4e680fbd31b732bdf
ase2022-causal <105044794+ase2022-causal@users.noreply.github.com>
2022-05-06 23:09:59 +0800

    Update name_test.sh

--- function naming/name_test.sh
+++ function naming/name_test.sh
@@ -8,3 +8,5 @@ output_dir=output/model_name
 dev_file=$data_dir/valid.jsonl
 test_file=$data_dir/test.jsonl
 test_model=$output_dir/checkpoint-best-bleu/pytorch_model.bin #checkpoint for test
+
+python run_causal.py --do_test --model_type roberta --model_name_or_path microsoft/codebert-base --load_model_path $test_model --dev_filename $dev_file --test_filename $test_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --eval_batch_size $batch_size
